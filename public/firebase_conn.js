4d13b4f8dbe941e571f355cecfb33bb333f95d30
astaadmnn <altunap@students.nu-fairview.edu.ph>
2025-10-02 11:52:58 +0800

    Update firebase_conn.js

--- public/firebase_conn.js
+++ public/firebase_conn.js
@@ -17,3 +17,4 @@ const firebaseConfig = {
 const app = initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
 export const db = getDatabase(app);
+export const storage = getStorage(app);
